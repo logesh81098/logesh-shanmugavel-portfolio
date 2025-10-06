83421851f70515e672a6fc3c36f53b777b57be66
Logesh Shanmugavel <logesh81098@gmail.com>
2025-10-07 03:00:51 +0530

    Uploaded index.html, Resume and Image to Portfolio bucket

--- module/s3/main.tf
+++ module/s3/main.tf
@@ -10,3 +10,23 @@ resource "aws_s3_bucket" "logesh-portfolio" {
   }
 }
 
+#Upload files to S3 bucket
+
+resource "aws_s3_object" "upload-index-file" {
+  bucket = aws_s3_bucket.logesh-portfolio.id
+  source = "module/s3/index.html"
+  key = "index.html"
+  content_type = "text/html"
+}
+
+resource "aws_s3_object" "upload-resume" {
+  bucket = aws_s3_bucket.logesh-portfolio.id
+  source = "module/s3/Logesh Updated Resume.pdf"
+  key = "Logesh Updated Resume.pdf"
+}
+
+resource "aws_s3_object" "upload-image" {
+  bucket = aws_s3_bucket.logesh-portfolio.id
+  source = "module/s3/Logesh.jpeg"
+  key = "Logesh.jpeg"
+}
